e9965894d0b45560fa5a7baf5652977bf30bfc1b
nasimali <22627654+nasimali@users.noreply.github.com>
2023-12-23 14:54:07 +0000

    feat(RIJ-10): fixed web edge function request

--- supabase/functions/listUserByEmail/index.ts
+++ supabase/functions/listUserByEmail/index.ts
@@ -4,19 +4,31 @@
 // You'll need to include the necessary Supabase libraries
 // and your Supabase service key in a secure manner.
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { corsHeaders } from "../_shared/cors.ts";
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = Deno.env.get('SUPABASE_URL');
+const supabaseAnonKey = Deno.env.get('SUPABASE_ANON_KEY');
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 Deno.serve(async (req) => {
+
+  if (req.method === 'OPTIONS') {
+    return new Response(null, {
+      status: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Authorization, Content-Type',
+      },
+    });
+  }
   const { email } = await req.json();
 
   if (!email) {
     return new Response(
       JSON.stringify({ error: 'Email is required' }),
-      { status: 400, headers: { "Content-Type": "application/json" } },
+      { status: 400, headers: { ...corsHeaders, "Content-Type": "application/json" } },
     );
   }
 
@@ -28,25 +40,25 @@ Deno.serve(async (req) => {
   if (error) {
     return new Response(
       JSON.stringify({ error: 'Internal Server Error' }),
-      { status: 500, headers: { "Content-Type": "application/json" } },
+      { status: 500, headers: { ...corsHeaders, "Content-Type": "application/json" } },
     );
   }
 
   if (data.length === 1) {
     return new Response(
       JSON.stringify({ userExists: true }),
-      { headers: { "Content-Type": "application/json" } },
+      { headers: { ...corsHeaders, "Content-Type": "application/json" } },
     );
   } else if (data.length === 0) {
     return new Response(
       JSON.stringify({ userExists: false }),
-      { headers: { "Content-Type": "application/json" } },
+      { headers: { ...corsHeaders, "Content-Type": "application/json" } },
     );
   } else {
     // If more than one row is found, return an error
     return new Response(
       JSON.stringify({ error: 'Multiple users found with the same email' }),
-      { status: 400, headers: { "Content-Type": "application/json" } },
+      { status: 400, headers: { ...corsHeaders, "Content-Type": "application/json" } },
       );
   }
 });
